9fa02e9f9793d0adb483a8f53435c1cdf595f476
Tapio Vierros <tapiovierros@gmail.com>
2015-01-29 20:31:10 +0200

    Rudimentary command parsing draft.

--- tests/tool.cpp
+++ tests/tool.cpp
@@ -11,6 +11,24 @@
 using namespace gentex;
 using namespace json11;
 
+typedef std::function<vec4(vec4, vec4)> CompositeFunction;
+
+struct Op {
+	std::string name;
+	CompositeFunction op;
+};
+
+const std::vector<Op> s_ops = {
+	{ "set", [](vec4  , vec4 b) { return b; } },
+	{ "add", [](vec4 a, vec4 b) { return a + b; } },
+	{ "sub", [](vec4 a, vec4 b) { return a - b; } },
+	{ "mul", [](vec4 a, vec4 b) { return a * b; } },
+	{ "div", [](vec4 a, vec4 b) { return a / b; } },
+	{ "min", [](vec4 a, vec4 b) { return min(a, b); } },
+	{ "max", [](vec4 a, vec4 b) { return max(a, b); } },
+};
+
+
 std::string readFile(const std::string& path) {
 	std::ifstream f(path);
 	return std::string(std::istreambuf_iterator<char>(f), std::istreambuf_iterator<char>());
@@ -30,7 +48,18 @@ int main(int /*argc*/, char** argv) {
 	int w = spec["size"][0].int_value();
 	int h = spec["size"][1].int_value();
 	Image tex(w, h);
-	solidColor(tex, vec3(0.0, 0.2, 0.3));
+
+	const auto& cmds = spec["ops"].array_items();
+	for (const auto& cmd : cmds) {
+		for (const auto& op : s_ops) {
+			if (!cmd[op.name].is_null()) {
+				const std::string& gen = cmd[op.name].string_value();
+				std::cout << "Applying " << gen << " with " << op.name << std::endl;
+				break;
+			}
+		}
+	}
+
 	tex.writeTGA(spec["out"].string_value());
 
 	return 0;
