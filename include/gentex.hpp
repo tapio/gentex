ec83f0756f0c9378043e356e1aad6a6bb91919e9
Tapio Vierros <tapiovierros@gmail.com>
2015-01-29 20:33:39 +0200

    Use vec4 instead of vec3.

--- include/gentex.hpp
+++ include/gentex.hpp
@@ -8,7 +8,7 @@ namespace gentex {
 
 // Generators
 
-inline void solidColor(Image& img, const vec3& color) {
+inline void solidColor(Image& img, const vec4& color) {
 	img.generate([color](int, int) { return color; });
 }
 
@@ -19,17 +19,17 @@ inline void solidColor(Image& img, const vec3& color) {
 // Filters
 
 inline void grayscaleAverage(Image& img) {
-	img.filter([](int, int, vec3 color) {
+	img.filter([](int, int, vec4 color) {
 		float gray = (color.r + color.g + color.b) / 3.0;
-		return vec3(gray);
+		return vec4(gray);
 	});
 }
 
 inline void grayscaleLuminance(Image& img) {
-	img.filter([](int, int, vec3 color) {
+	img.filter([](int, int, vec4 color) {
 		// http://en.wikipedia.org/wiki/Grayscale
 		float gray = 0.2126 * color.r + 0.7152 * color.g + 0.0722 * color.b;
-		return vec3(gray);
+		return vec4(gray);
 	});
 }
 
--- include/image.hpp
+++ include/image.hpp
@@ -8,47 +8,47 @@
 #define GLM_FORCE_CXX11
 #include <glm/glm.hpp>
 
-using glm::vec3;
+using glm::vec4;
 
 class Image {
 public:
-	typedef std::function<vec3(int, int, vec3)> FilterFunction;
-	typedef std::function<vec3(int, int)> GeneratorFunction;
+	typedef std::function<vec4(int, int, vec4)> FilterFunction;
+	typedef std::function<vec4(int, int)> GeneratorFunction;
 
 	Image(int w, int h): w(w), h(h), buffer(w * h) { }
 
-	vec3 sample(float u, float v) {
+	vec4 sample(float u, float v) {
 		return get(u / w, v / h);
 	}
 
-	vec3 sampleClamp(float u, float v) {
+	vec4 sampleClamp(float u, float v) {
 		u = u < 0 ? 0 : (u > 1.0 ? 1.0 : u);
 		v = v < 0 ? 0 : (v > 1.0 ? 1.0 : v);
 		return get(u / w, v / h);
 	}
 
-	vec3 sampleRepeat(float u, float v) {
+	vec4 sampleRepeat(float u, float v) {
 		return get(int(u / w) % w, int(v / h) % h);
 	}
 
-	vec3 get(int x, int y) {
+	vec4 get(int x, int y) {
 		return buffer[x + y * w];
 	}
 
-	vec3 getClamp(int x, int y) {
+	vec4 getClamp(int x, int y) {
 		x = x < 0 ? 0 : (x > w ? w : x);
 		y = y < 0 ? 0 : (y > h ? h : y);
 		return get(x, y);
 	}
 
-	vec3 getRepeat(int x, int y) {
+	vec4 getRepeat(int x, int y) {
 		return get(x % w, y % h);
 	}
 
 	void filter(FilterFunction func) {
 		for (int y = 0; y < h; ++y) {
 			for (int x = 0; x < w; ++x) {
-				vec3& color = buffer[y * w + x];
+				vec4& color = buffer[y * w + x];
 				color = func(x, y, color);
 			}
 		}
@@ -83,7 +83,7 @@ public:
 		// Image data
 		for (int y = h-1; y >= 0; --y) {
 			for (int x = 0; x < w; ++x) {
-				const vec3& pix = get(x, y);
+				const vec4& pix = get(x, y);
 				tgaout << static_cast<char>(pix.b * 255);
 				tgaout << static_cast<char>(pix.g * 255);
 				tgaout << static_cast<char>(pix.r * 255);
@@ -92,5 +92,5 @@ public:
 	}
 
 	int w, h;
-	std::vector<vec3> buffer;
+	std::vector<vec4> buffer;
 };
